a80c15c3c1e9f32592daa02f0fb18cdfd0b16f31
AdirtKa <AdirtKa@yandex.ru>
2023-10-12 21:05:36 +1000

    .

--- digits/keras_digits.py
+++ digits/keras_digits.py
@@ -4,7 +4,7 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = '2'
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras
-from keras.layers import Dense, Flatten
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from keras.datasets import mnist
 
 
@@ -18,9 +18,12 @@ y_train_cat = keras.utils.to_categorical(y_train, 10)
 y_test_cat = keras.utils.to_categorical(y_test, 10)
 
 model = keras.Sequential([
+    Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D((2, 2), strides=2),
+    Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D((2, 2), strides=2),
     Flatten(input_shape=(28, 28, 1)),
     Dense(90, activation='relu'),
-    Dense(70, activation='relu'),
     Dense(10, activation='softmax'),
 ])
 

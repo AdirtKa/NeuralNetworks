f71d55f00c625adb8d5b23e55c50a2cfc495aaaa
AdirtKa <AdirtKa@yandex.ru>
2023-10-15 20:06:30 +1000

    .

--- face_recognition/main.py
+++ face_recognition/main.py
@@ -2,15 +2,16 @@ import cv2
 from time import sleep
 
 def face_capture():
-    cascade_path = r"D:\Projects\NeuralNetworks\face_recognition\haarcascade_frontalface_default.xml"
+    cascade_path = r"E:\Projects\NeuralNetworks\face_recognition\haarcascade_frontalface_default.xml"
+
+    input_path = r"E:\Projects\NeuralNetworks\face_recognition\1.jpg"   
 
     clf = cv2.CascadeClassifier(cascade_path)
-    camera = cv2.VideoCapture(0)
+    camera = cv2.VideoCapture(input_path)
 
-    while True:
-        _, frame = camera.read()
+    if input_path.endswith(('.jpg', '.jpeg', '.png', '.bmp')):
+        frame = cv2.imread(input_path)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
         faces = clf.detectMultiScale(
             gray,
             scaleFactor=1.1,
@@ -18,16 +19,34 @@ def face_capture():
             minSize=(30, 30),
             flags=cv2.CASCADE_SCALE_IMAGE
         )
-
         for (x, y, width, height) in faces:
             cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 255, 0), 2)
-
         cv2.imshow('Faces', frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
 
-        if cv2.waitKey(1) == ord('q'):
-            break
+        while True:
+            _, frame = camera.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        sleep(0.01)
+            faces = clf.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+
+            for (x, y, width, height) in faces:
+                cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 255, 0), 2)
+
+            cv2.imshow('Faces', frame)
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+            sleep(0.01)
 
         
     camera.release()
